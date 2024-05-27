c3e79f77479ce514b09b7193cba2c4e000857125
Nikita <nikita.voloboev@gmail.com>
2024-05-27 13:36:08 +0200

    .

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+	title: "nikiv.dev",
+	description: "Personal site of Nikita Voloboev",
+}
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	)
+}
