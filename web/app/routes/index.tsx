b63a4cbd3ec0539a58a36a32e4f899d97bc77ac7
Nikita <github@nikiv.dev>
2025-01-05 20:52:20 +0100

    .

--- web/app/routes/index.tsx
+++ web/app/routes/index.tsx
@@ -18,8 +18,9 @@ function RouteComponent() {
           you want to talk.
         </p>
         <p>
-          I have a <a href="https://docs.nikiv.dev">wiki</a> of things I know or
-          liked.
+          I have a <a href="https://docs.nikiv.dev">wiki</a> of{" "}
+          <a href="https://docs.nikiv.dev/learn">things I know</a> or
+          <a href="https://docs.nikiv.dev/likes">liked</a>.
         </p>
       </div>
     </>
